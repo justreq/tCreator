559d550bbe6d60fabf75035e86269663c98298ed
Req <justreqofficial@gmail.com>
2023-11-15 22:09:38 -0500

    Title screen and mod creation guide complete

--- Scripts/TitleScreen.gd
+++ Scripts/TitleScreen.gd
@@ -1,6 +1,25 @@
 extends Control
 
-@onready var TitleScreenContent = $PanelContainer/TitleScreenContent
+var creatingAModGuideVisible = false: set = setCreatingAModGuideVisible
+
+@onready var titleScreenContent = $PanelContainer/TitleScreenContent
+@onready var creatingAModGuideContent = $PanelContainer/CreatingAModGuideContent
+
+func _process(_delta):
+	if Input.is_action_just_pressed("ui_cancel") && creatingAModGuideVisible:
+		creatingAModGuideVisible = false
+
+func setCreatingAModGuideVisible(state):
+	creatingAModGuideVisible = state
+	titleScreenContent.visible = !state
+	creatingAModGuideContent.visible = state
 
 func onCreatingAModButtonPressed():
-	TitleScreenContent.visible = false
+	creatingAModGuideVisible = true
+
+func onExitButtonPressed():
+	creatingAModGuideVisible = false
+
+
+func onRichTextLabelMetaClicked(meta):
+	OS.shell_open(meta)
